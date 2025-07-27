8da3fe8c1ade7da909d1aa8233d057f8e9f3b95b
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-27 23:44:48 +0800

    Updating with typing

--- src/ihep_utilities/__init__.py
+++ src/ihep_utilities/__init__.py
@@ -1,14 +1,22 @@
 '''
 This should put all the imports in the namespace
 '''
+from typing import TYPE_CHECKING, Type, Any
+
+if TYPE_CHECKING:
+    from .job_submitter import JobSubmitter
 
 __all__ = ['JobSubmitter']
 # ----------------------
-def __getattr__(name : str) -> None:
+def __getattr__(name : str) -> Type[Any]:
     '''
     Parameters
     -------------
-    name: Name of module to import
+    name: Name of class to import
+
+    Return
+    -------------
+    Class to be imported
     '''
     if name not in __all__:
         raise ValueError(f'Module {name} not found among: {__all__}')
