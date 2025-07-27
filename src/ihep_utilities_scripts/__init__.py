9f7c6739971239b18bcbc8cd9b4a3b22df914ac2
Angel Campoverde <angel.campoverde@cern.ch>
2025-07-27 22:53:52 +0800

    Adding files

---
+++ src/ihep_utilities_scripts/__init__.py
@@ -0,0 +1,1 @@
+from job_submitter import JobSubmitter
---
+++ src/tests/test_job_submitter.py
@@ -0,0 +1,50 @@
+'''
+Script holding functions that test JobSubmitter class
+'''
+import os
+
+# ----------------------
+class Data:
+    '''
+    Class meant to be used to share attributes
+    '''
+    user    = os.environ['USER']
+    out_dir = f'/tmp/{user}/ihep_utilities/tests'
+# ----------------------
+def _get_path(kind : str) -> str:
+    '''
+    Parameters
+    -------------
+    kind: Type of job submission
+
+    Returns
+    -------------
+    Path to text file with commands to run as jobs
+    '''
+    l_command = []
+    if kind == 'echo':
+        l_command.append('echo 1')
+        l_command.append('echo 2')
+        l_command.append('echo 3')
+
+    data = '\n'.join(l_command)
+    path = f'{Data.out_dir}/{kind}.txt'
+    with open(path, 'w', encoding='utf-8') as ofile:
+        ofile.write(data)
+
+    return path
+# ----------------------
+def test_simple() -> None:
+    '''
+    Parameters
+    -------------
+    None
+
+    Returns
+    -------------
+    None
+    '''
+    path = _get_path(kind='echo')
+
+    sbt = JobSubmitter(path=path, environment='None')
+    sbt.run() 
